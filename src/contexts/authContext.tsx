23b8e4c503a1b18087aa8f094cc8627cf4807e32
martinezmauri <maurimartine01@gmail.com>
2024-11-09 03:04:39 -0300

    9/11 primer deploy

--- src/contexts/authContext.tsx
+++ src/contexts/authContext.tsx
@@ -6,7 +6,6 @@ import withReactContent from "sweetalert2-react-content";
 import { useSession, signOut } from "next-auth/react";
 import axios from "axios";
 import { jwtDecode } from "jwt-decode";
-import { useRouter } from "next/router";
 
 const MySwal = withReactContent(Swal);
 
@@ -37,7 +36,7 @@ export const AuthProvider = ({ children }: IAuthProviderProps) => {
   const userCreatedRef = useRef(false);
 
   const createUserExternal = useCallback(async () => {
-    const url = "http://localhost:3001";
+    const url = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001";
     const storedUser = sessionStorage.getItem("userDevNavigator");
     const existingUser = storedUser ? JSON.parse(storedUser) : null;
 
